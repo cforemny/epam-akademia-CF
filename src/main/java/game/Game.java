587c2db27c7769530523fd7acf74a55e9863f10c
Cyprian <cforemny>
2017-05-29 13:14:05 +0200

    5th commit

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -5,7 +5,6 @@ import utils.Mark;
 import utils.Omark;
 import utils.Xmark;
 
-import javax.swing.*;
 import java.util.Scanner;
 
 /**
@@ -13,37 +12,53 @@ import java.util.Scanner;
  */
 public class Game {
 
-    private Board board;
+    private Board board = new Board();
     private WinConditiion winCondition;
-    private Scanner scanner;
+    private Scanner scanner = new Scanner(System.in);;
     private String columnPosition;
     private String recordPosition;
 
 
 
-
-    public void playTheGame(Mark mark){
+    public void playTheGame() {
 
         WinConditiion winConditiion = new WinConditiion();
-        do{
+
+        Mark mark = new Xmark();
+        do {
             System.out.println("Choose the column");
             columnPosition = scanner.nextLine();
             int columnPositionInt = Integer.parseInt(columnPosition);
-            System.out.println("Choose the column");
+            System.out.println("Choose the record");
             recordPosition = scanner.nextLine();
             int recordPositionInt = Integer.parseInt(recordPosition);
-            if(mark.equals('x')){
-                board.fillTheBoardWithXmark(columnPositionInt+1,recordPositionInt+1);
+            if (mark instanceof Xmark) {
+                board.fillTheBoardWithXmark(columnPositionInt , recordPositionInt );
+                mark = new Omark();
             }
-            if(mark.equals('o')){
-                board.fillTheBoardWithOMark(columnPositionInt+1,recordPositionInt+1);
+            if (mark instanceof Omark) {
+                board.fillTheBoardWithOMark(columnPositionInt , recordPositionInt );
+                mark = new Xmark();
             }
-        }while(winConditiion.isTheGameEnd(board));
+
+            board.getTablicaView()[0][0] = ' ';
+            board.getTablicaView()[0][1] = '1';
+            board.getTablicaView()[0][2] = '2';
+            board.getTablicaView()[0][3] = '3';
+            board.getTablicaView()[1][0] = '1';
+            board.getTablicaView()[2][0] = '2';
+            board.getTablicaView()[3][0] = '3';
+            for (int i = 0; i < board.getTablicaView().length; i++) {
+                for (int j = 0; j < board.getTablicaView()[i].length; j++)
+                    System.out.print(board.getTablicaView()[i][j] + " ");
+                System.out.println();
+            }
+            System.out.println();
+
+        } while (true);
 
 
-       board.createBoardView();
     }
-
 
 
 }
